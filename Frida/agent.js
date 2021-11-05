b2487e2613452fe570afeb2a55212ab9885169c9
Draguve <ritwikpatil1998@gmail.com>
2021-11-06 04:22:35 +0530

    stacktrace for all the .rec access

--- Frida/agent.js
+++ Frida/agent.js
@@ -1,28 +1,24 @@
-
-console.log(JSON.stringify(Process.enumerateModules(),null,4));
-
+// console.log(JSON.stringify(Process.enumerateModules(), null, 4));
 const ReadFile = Module.getExportByName(null, "ReadFile");
 
 const MAXSIZE = 512;
-var GetFinalPathNameByHandleA = new NativeFunction( Module.getExportByName(null, 'GetFinalPathNameByHandleA'), "uint32", ["pointer","pointer","uint32","uint32"]);
+var GetFinalPathNameByHandleA = new NativeFunction(Module.getExportByName(null, 'GetFinalPathNameByHandleA'), "uint32", ["pointer", "pointer", "uint32", "uint32"]);
 
 Interceptor.attach(ReadFile, {
-  onEnter(args) {
-    // console.log("--------------------------")
-    // for(let x=0;x<10;x++){
-    //   console.log(args[x]);
-    // }
-    
-    var fileName = Memory.alloc(MAXSIZE);
-    var size = GetFinalPathNameByHandleA(args[0],fileName,MAXSIZE,0);
-    if(size){
-      console.log(size);
-      console.log(hexdump(fileName, { offset: 0,  length: size,  header: true, ansi: false}));
-    }
-    // console.log("ReadFile called from : \n" +
-    //     Thread.backtrace(this.context, Backtracer.ACCURATE)
-    //     .map(DebugSymbol.fromAddress).join('\n') + '\n');
-  }
+    onEnter(args) {
+        var fileName = Memory.alloc(MAXSIZE);
+        var size = GetFinalPathNameByHandleA(args[0], fileName, MAXSIZE, 0);
+        if (size) {
+            var name = fileName.readAnsiString(size);
+            if (name.endsWith(".rec")) {
+                console.log(name);
+                console.log("ReadFile called from : \n" +
+                    Thread.backtrace(this.context, Backtracer.ACCURATE)
+                    // .map(DebugSymbol.fromAddress)
+                    .join('\n') + '\n');
+            }
+        }
+	}
 });
 
 // frida -n RainbowSix.exe -l .\Frida\agent.js
